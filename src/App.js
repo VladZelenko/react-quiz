f618d40be01bdfcc3daac3482d8569e8fb28531a
Vlad6555 <Vlad6555@ukr.net>
2019-07-04 10:12:42 +0300

    Signed-off-by: Vlad6555 <Vlad6555@ukr.net>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Layout from './hoc/Layout/Layout';
+import Layout from './hoc/Layout/Layout'
 
 function App() {
   return (
     <Layout>
       <div style={{width: 400, border: '1px solid black'}}>
-        <h1>Layout</h1>
+        <h1>Hello</h1>
       </div>
     </Layout>
   );
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,16 +1,15 @@
 import React, {Component} from 'react'
-import classes from './Layout.css'
 
-class Layout extends Component {
+  class Layout extends Component {
     render() {
-        return (
-            <div className={classes.Layout}>
-                <main>
-                    { this.props.children }
-                </main>
-            </div>
-        )
-    }   
-}
+      return (
+        <div>
+          <main>
+            { this.props.children }
+          </main>
+        </div>
+      )
+    }
+  }
 
-export default Layout
+  export default Layout
