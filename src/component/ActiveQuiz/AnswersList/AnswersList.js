ffd3fa9386b4343f8b978379b2f57be54cb6ff99
Vlad6555 <Vlad6555@ukr.net>
2019-07-05 12:46:39 +0300

    lesson 7 add event

--- src/component/ActiveQuiz/AnswersList/AnswersList.js
+++ src/component/ActiveQuiz/AnswersList/AnswersList.js
@@ -1,11 +1,17 @@
 import React from "react";
 import classes from "./AnswersList.module.css";
-import AnswerItem from "./AnswerItem/AnswerIten";
+import AnswerItem from "./AnswerItem/AnswerItem";
 
 const AnswersList = props => (
   <ul className={classes.AnswersList}>
-    {props.answers.map((answer, index) => {
-      return <AnswerItem answer={answer} />;
+    {props.answers.map((value, key) => {
+      return (
+        <AnswerItem
+          answer={value}
+          key={key}
+          onAnswerClick={props.onAnswerClick}
+        />
+      );
     })}
   </ul>
 );
--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -4,15 +4,40 @@ import ActiveQuiz from "../../component/ActiveQuiz/ActiveQuiz";
 
 class Quiz extends Component {
   state = {
-    quiz: []
+    quiz: [
+      {
+        question: "Какого цвета небо?",
+        rightAnswerId: 2,
+        answers: [
+          { text: "Красное", id: 1 },
+          { text: "Синее", id: 2 },
+          { text: "Белое", id: 3 },
+          { text: "Зеленое", id: 4 }
+        ]
+      }
+    ],
+    title: "Ответьте на все вопросы."
+  };
+
+  onAnswerClickHendler = answerId => {
+    if (answerId === 2) {
+      console.log("Правильный ответ");
+    } else {
+      console.log("Подумай еще");
+    }
+    // console.log("ID " + answerId);
   };
 
   render() {
     return (
       <div className={classes.Quiz}>
         <div className={classes.wrapper}>
-          <h1>Quiz</h1>
-          <ActiveQuiz answers={this.state} />
+          <h1>{this.state.title}</h1>
+          <ActiveQuiz
+            answers={this.state.quiz[0].answers}
+            question={this.state.quiz[0].question}
+            onAnswerClick={this.onAnswerClickHendler}
+          />
         </div>
       </div>
     );
